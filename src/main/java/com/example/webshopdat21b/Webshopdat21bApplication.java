08f78879608cd8652f08ea766d2c621d740af0f2
jarltuxen <jarl.tuxen@gmail.com>
2022-03-21 14:57:59 +0100

    Initial commit

---
+++ src/main/java/com/example/webshopdat21b/Webshopdat21bApplication.java
@@ -0,0 +1,13 @@
+package com.example.webshopdat21b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webshopdat21bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webshopdat21bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webshopdat21b/Webshopdat21bApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webshopdat21b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webshopdat21bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
