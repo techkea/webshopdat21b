faee9a0e0f82465706b15ac9d111b54d9704dc54
jarltuxen <jarl.tuxen@gmail.com>
2022-03-21 15:14:52 +0100

    Repository - half version

---
+++ src/main/java/com/example/webshopdat21b/repository/ProductRepository.java
@@ -0,0 +1,43 @@
+package com.example.webshopdat21b.repository;
+
+import com.example.webshopdat21b.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+
+    public List<Product> getAll() {
+        List<Product> productList = new ArrayList<>();
+        Connection con = getConnection();
+        try {
+
+            Statement s = con.createStatement();
+            final String SQL_QUERY = "SELECT * FROM product"; //" WHERE id = 1 OR 1=1; --";
+            ResultSet rs = s.executeQuery(SQL_QUERY);
+
+
+            //read data from resultset
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                int price = rs.getInt(3);
+                //System.out.println(id + " " + name + " " + price);
+                productList.add(new Product(id, name, price));
+            }
+
+            s.close();
+        } catch (
+                SQLException e) {
+            System.out.println("Could not create connection");
+            e.printStackTrace();
+        }
+        return productList;
+    }
+}
