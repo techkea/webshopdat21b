73d5a51f6dd24ccbafd39a9bba5e902b7d566695
jarltuxen <jarl.tuxen@gmail.com>
2022-03-21 15:34:46 +0100

    Repository - med connection

--- src/main/java/com/example/webshopdat21b/repository/ProductRepository.java
+++ src/main/java/com/example/webshopdat21b/repository/ProductRepository.java
@@ -3,22 +3,43 @@ package com.example.webshopdat21b.repository;
 import com.example.webshopdat21b.model.Product;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class ProductRepository {
 
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/webshop";
+    private final static String UID = "root";
+    private final static String PWD = "qJiw03K2zwJD";
+
+    Connection connection = null;
+
+    //get connection
+    public Connection getConnection(){
+        //connection er en singleton
+        //connection already initialized?
+        if (connection!=null) return connection;
+
+        //initialize connection
+        try{
+            connection = DriverManager.getConnection(DB_URL, UID, PWD);
+
+        }
+        catch (SQLException e){
+            System.out.println("Could not connect");
+            e.printStackTrace();
+        }
+        return connection;
+
+    }
     public List<Product> getAll() {
         List<Product> productList = new ArrayList<>();
-        Connection con = getConnection();
+        getConnection();
         try {
 
-            Statement s = con.createStatement();
+            Statement s = connection.createStatement();
             final String SQL_QUERY = "SELECT * FROM product"; //" WHERE id = 1 OR 1=1; --";
             ResultSet rs = s.executeQuery(SQL_QUERY);
 
@@ -30,6 +51,7 @@ public class ProductRepository {
                 int price = rs.getInt(3);
                 //System.out.println(id + " " + name + " " + price);
                 productList.add(new Product(id, name, price));
+
             }
 
             s.close();
