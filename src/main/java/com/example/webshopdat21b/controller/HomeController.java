0f77c2197022e98f44798d54e82fc47a218a9218
jarltuxen <jarl.tuxen@gmail.com>
2022-03-21 15:53:52 +0100

    controller

--- src/main/java/com/example/webshopdat21b/controller/HomeController.java
+++ src/main/java/com/example/webshopdat21b/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.webshopdat21b.controller;
 
+import com.example.webshopdat21b.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    ProductRepository productRepository;
+
+    public HomeController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @GetMapping("/")
     public String index(Model model){
+        model.addAttribute("products", productRepository.getAll());
         return "index";
     }
 }
